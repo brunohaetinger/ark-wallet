daf3a8275d7a74f465a51ddf8c1a431048909414
Bruno Haetinger <brunohaetinger@gmail.com>
2021-04-02 23:19:21 -0300

    Add tailwind sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
 	purge: [],
 	theme: {
-		extend: {},
+		extend: {
+			height: {
+				13: "3.25rem",
+				15: "3.75rem",
+			},
+			padding: {
+				7.5: "1.875rem",
+			},
+		},
 	},
 	variants: {},
 	plugins: [],
