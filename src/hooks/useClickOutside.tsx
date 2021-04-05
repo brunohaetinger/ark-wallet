70eb6daa6c66f886e78875a663d818aaeb577716
Bruno Haetinger <brunohaetinger@gmail.com>
2021-04-05 20:36:20 -0300

    Add click outisde hook

---
+++ src/hooks/useClickOutside.tsx
@@ -0,0 +1,16 @@
+export const clickOutsideHandler = (ref: any, callback: any) => {
+	// Check if clicked outside of the ref element
+	const handleClickOutside = (event: any) => {
+		if (ref.current && !ref.current.contains(event.target)) {
+			if (typeof callback === "function") {
+				callback();
+			}
+		}
+	};
+
+	// Listen to click events
+	document.addEventListener("mousedown", handleClickOutside);
+
+	// Unbind listener on clean up
+	return () => document.removeEventListener("mousedown", handleClickOutside);
+};
