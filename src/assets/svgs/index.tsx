da25bd979f232983a0318fd1cbd63f9a1b39e4ab
Bruno Haetinger <brunohaetinger@gmail.com>
2021-04-05 20:35:30 -0300

    Move SVGs to specific folder

---
+++ src/assets/svgs/index.tsx
@@ -0,0 +1,20 @@
+// import Address from "./address.svg";
+// import ArrowDown from "./arrow-down.svg";
+// import ArrowUp from "./arrow-up.svg";
+// import Balance from "./balance.svg";
+// import Logo from "./logo.svg";
+// // import { ReactComponent as Address } from "./address.svg";
+// // import { ReactComponent as ArrowDown } from "./arrow-down.svg";
+// // import { ReactComponent as ArrowUp } from "./arrow-up.svg";
+// // import { ReactComponent as Balance } from "./balance.svg";
+// // import { ReactComponent as Logo } from "./logo.svg";
+
+// const SvgCollection = {
+// 	Logo,
+// 	Address,
+// 	ArrowDown,
+// 	ArrowUp,
+// 	Balance,
+// };
+
+// export default SvgCollection;
